2a14132610d3d79eeb73d39a3470700fe2a9b249
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-05-10 14:27:54 +0800

    ver 4

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,12 @@
 const config ={
     production:{
         SECRET: process.env.SECRET,
-        DATABASE: process.env.MONGODB_URI
+        // DATABASE: process.env.MONGODB_URI
+        DATABASE: 'mongodb://nbagonoc:FuckTh3G0v3rnm3nt@ds119640.mlab.com:19640/nb-mern-stack'
     },
     default:{
         SECRET: 'qwerty123',
-        // DATABASE: 'mongodb://localhost:27017/bookshelf'
-        DATABASE: 'mongodb://nbagonoc:FuckTh3G0v3rnm3nt@ds119640.mlab.com:19640/nb-mern-stack'
+        DATABASE: 'mongodb://localhost:27017/bookshelf'
     }
 }
 
